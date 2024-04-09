fa1dc28664f2f8d1235772d4a5f67412cb07e193
Vinni Uba <20180015014@my.xu.edu.ph>
2024-04-09 15:23:23 +0800

    Update cpu_db.sql

--- cpu_db.sql
+++ cpu_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2024 at 09:02 AM
+-- Generation Time: Apr 09, 2024 at 09:23 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
