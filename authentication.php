6afe0bb9f760d0952abc3eec39bc6e453d17048e
Vinni Uba <20180015014@my.xu.edu.ph>
2024-05-15 10:51:11 +0800

    update

--- authentication.php
+++ authentication.php
@@ -2,7 +2,7 @@
 include('config/dbcon.php');
 if(!isset($_SESSION['auth']))
 {
-    $_SESSION['message'] = "Login to Access Website Nigga";
+    $_SESSION['message'] = "Login to Access Website";
     header('location: login.php');
     exit(0);
 }
